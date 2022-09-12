cd2e3a52d417a5072ec7bbb1d0c64e0a790f1f2a
dkswami <dkswami1820@gmail.com>
2022-09-12 18:13:05 +0530

    added post feature on home page

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -7,14 +7,39 @@ import { ReactComponent as FeelingIcon } from '../../assets/feeling.svg';
 import Post from "../../components/post/post.component";
 import  './home.styles.scss';
 
+const DefaultpostData = {
+	title: "",
+	description: ""
+}
+
 const Home = () => {
 	const [posts, setPosts] = useState([]);
+	const [postData, setPostData] = useState(DefaultpostData);
 
+	const { title, description } = postData;
+	
 	const getAllPosts = async () => {
 		try {
 			const response = await axios.get('http://localhost:3000/api/v1/posts');
 			console.log(response);
 			setPosts(response.data.data);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	const onChangeHandler = (event) => {
+		const desValue = event.target.value;
+		const titleValue = desValue.slice(0,10);
+
+		setPostData({title:`${titleValue}`, description:`${desValue}`});
+		console.log(postData);
+	}
+
+	const onClickPost = async () => {
+		try {
+			const response = await axios.post('http://localhost:3000/api/v1/posts',postData);
+			console.log(response);
 		} catch (error) {
 			console.error(error);
 		}
@@ -27,7 +52,7 @@ const Home = () => {
 	return (
 		<div className='home-container'>
 			<div className='add-post-container'>
-				<input type='text' placeholder="What's happening ?" />
+				<input type='text' placeholder="What's happening ?   First 10 Characters will be taken as title."  onChange={onChangeHandler} />{console.log(description)}
 				<div className='addons-with-post'>
 					<div className='add-post-item'>
 						<LiveVideoIcon /><span> Live Video</span>
@@ -39,7 +64,7 @@ const Home = () => {
 						<FeelingIcon /> <span> Feeling </span>
 					</div>					
 				</div>
-				<button className='add-post-button'>Post</button>
+				<button className='add-post-button' onClick={onClickPost }>Post</button>
 			</div>
 
 			{
