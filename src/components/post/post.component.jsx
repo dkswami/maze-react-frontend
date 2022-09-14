dcdc2e1d5d9ceced532c2273827af10693a1d75b
dkswami <dkswami1820@gmail.com>
2022-09-14 12:31:48 +0530

    new comment add feature added successfully

--- src/components/post/post.component.jsx
+++ src/components/post/post.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import { ReactComponent as ThreeDots } from '../../assets/three-dots.svg';
 import { ReactComponent as SingleDot } from '../../assets/single-dot.svg';
 import { ReactComponent as LikeIcon } from '../../assets/like.svg';
@@ -8,10 +9,39 @@ import ActionDropdown from '../action-dropdown/action-dropdown.component';
 
 import './post.styles.scss';
 
+const DefaultnewCommentData = {
+	body: "",
+	post_id: ""
+}
+
 const Post = ({ post }) => {
 	const [isActionDropdownOpen, setIsActionDropdownOpen] = useState(false);
-
 	const toggleActionDropdown = () => setIsActionDropdownOpen(!isActionDropdownOpen);
+
+	const [ newCommentData, setNewCommentData ] = useState(DefaultnewCommentData);
+
+	const onCommentInputChangeHandler = (event) => {
+		const bodyValue = event.target.value;
+		const idValue = post.id;
+
+		setNewCommentData({ body: `${bodyValue}`, post_id: `${idValue}` });
+		console.log(newCommentData);
+	}
+
+
+	const addCommentHandler = async () => {
+		if (newCommentData == DefaultnewCommentData) {
+			alert("please enter something in comment!");
+		} else {
+			try {
+				const response = await axios.post('http://localhost:3000/api/v1/comments', newCommentData);
+				alert("New comment added successfully!");
+				window.location.reload();
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
 
 	return (
 		<>
@@ -43,8 +73,8 @@ const Post = ({ post }) => {
 			</div>
 			<hr className='line2' />
 			<div className='add-a-comment'>
-				<input type='text' placeholder='  Write a comment' />
-				<div className='add-comment-button'>
+				<input type='text' placeholder='  Write a comment' onChange={onCommentInputChangeHandler} />
+				<div className='add-comment-button' onClick={addCommentHandler}>
 					<ArrowIcon className='arrow-icon' />
 				</div>
 			</div>
