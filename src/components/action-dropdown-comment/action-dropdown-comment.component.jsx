151a48a905d60c1139e9f2cd04634cc6a5e0aa5b
dkswami <dkswami1820@gmail.com>
2022-09-21 14:48:20 +0530

    comment edit feature added also modal used from react components

--- src/components/action-dropdown-comment/action-dropdown-comment.component.jsx
+++ src/components/action-dropdown-comment/action-dropdown-comment.component.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 import { ReactComponent as DeleteIcon } from '../../assets/delete-icon.svg';
 import { ReactComponent as EditIcon } from '../../assets/edit-post.svg';
 
 
+const DefaultupdatedComment = {
+	body: ""
+}
+
 const ActionDropdownComment = ({ comment }) => {
+	const [show, setShow] = useState(false);
+	const [updatedComment, setUpdatedComment ] = useState(DefaultupdatedComment);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+
 	console.log(comment.id);
 	const deleteHandler = async () => {
 		if (window.confirm("Do you want to delete this comment?")) {
@@ -20,23 +35,58 @@ const ActionDropdownComment = ({ comment }) => {
 		}
 	}
 
-	const editHandler = async () => {
+	const onChangeHandler = (event) => {
+		const bodyValue = event.target.value;
+		setUpdatedComment({ body:`${bodyValue}`});
+	}
 
+	const editHandler = async () => {
+		try {
+			const response = await axios.patch(`http://localhost:3000/api/v1/comments/${comment.id}`, updatedComment);
+			alert("Comment saved successfully!");
+			window.location.href = '/';
+		} catch (error) {
+			console.error(error);
+		}
+		handleClose();
 	}
 
 	return (
-		<div className='action-dropdown-container'>
-			<div className='delete-action' onClick={deleteHandler}>
-				<DeleteIcon />
-				<span>Delete</span>
-			</div>
-			{/* <Link> */}
-				<div className='edit-action'>
+		<>
+			<div className='action-dropdown-container'>
+				<div className='delete-action' onClick={deleteHandler}>
+					<DeleteIcon />
+					<span>Delete</span>
+				</div>
+				<div className='edit-action' onClick={handleShow}>
 					<EditIcon />
 					<span>Edit</span>
 				</div>
-			{/* </Link> */}
-		</div>
+			</div>
+			<Modal show={show} onHide={editHandler}>
+				<Modal.Header >
+					<Modal.Title>Edit Comment</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1"	>
+							<Form.Label>Example textarea</Form.Label>
+							<Form.Control as="textarea" rows={3} defaultValue={comment.attributes.body} onChange={onChangeHandler} autoFocus/>
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={editHandler}>
+						Save Changes
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+
+
 	)
 }
 
